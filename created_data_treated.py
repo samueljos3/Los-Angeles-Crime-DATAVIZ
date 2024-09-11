57163fefe8320ab751316a8aa4e78221491d78fd
Samuel <samukjo001@gmail.com>
2024-09-11 18:53:04 -0300

    update: novo tratamento de dados / novos gráficos implementados

--- created_data_treated.py
+++ created_data_treated.py
@@ -8,7 +8,7 @@ df = pd.read_csv('base/Crime_Data_from_2020_to_Present.csv')
 # Dropando colunas que não serão utilizadas
 df = df.drop(['Crm Cd', 'Crm Cd 1', 'Crm Cd 2', 'Crm Cd 3', 'Crm Cd 4',
               'Cross Street', 'LAT', 'LON', 'LOCATION', 'Mocodes', 'Premis Cd',
-              'AREA', 'TIME OCC', 'Rpt Dist No', 'Part 1-2', 'Weapon Used Cd'],
+              'AREA', 'Rpt Dist No', 'Part 1-2', 'Weapon Used Cd'],
              axis=1)
 
 # Removendo valores de Sexo da Vítima que não são válidos
@@ -24,6 +24,22 @@ df['YearMonth'] = df['DATE OCC'].dt.to_period('M').astype(str)
 # Removendo valores de Idade da Vítima que são menores ou iguais a 0
 df['Vict Age'] = pd.to_numeric(df['Vict Age'], errors='coerce')
 df = df[(df['Vict Age'] > 0) & (df['Vict Age'] < 110)]
+
+# Função para classificar os horários
+def classificar_periodo(time):
+    if 1 <= time <= 500:
+        return 'Madrugada'
+    elif 501 <= time <= 1200:
+        return 'Manhã'
+    elif 1201 <= time <= 1800:
+        return 'Tarde'
+    elif 1801 <= time <= 2359:
+        return 'Noite'
+    else:
+        return 'Desconhecido'
+
+# Aplicar a função a cada valor da coluna 'Time'
+df['Periodo'] = df['TIME OCC'].apply(classificar_periodo)
 
 # Salvando o DataFrame tratado em um arquivo CSV
 df.to_csv('base/Crime_Data_Treated.csv', index=False)
