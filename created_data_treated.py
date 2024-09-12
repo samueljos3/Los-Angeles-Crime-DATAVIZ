347c9fd7a7359c0f8aa0697a0b4466963a48ba9a
Samuel <samukjo001@gmail.com>
2024-09-12 12:50:08 -0300

    update: considerando dados de LAT e LON

--- created_data_treated.py
+++ created_data_treated.py
@@ -7,7 +7,7 @@ df = pd.read_csv('base/Crime_Data_from_2020_to_Present.csv')
 
 # Dropando colunas que não serão utilizadas
 df = df.drop(['Crm Cd', 'Crm Cd 1', 'Crm Cd 2', 'Crm Cd 3', 'Crm Cd 4',
-              'Cross Street', 'LAT', 'LON', 'LOCATION', 'Mocodes', 'Premis Cd',
+              'Cross Street', 'LOCATION', 'Mocodes', 'Premis Cd',
               'AREA', 'Rpt Dist No', 'Part 1-2', 'Weapon Used Cd'],
              axis=1)
 
